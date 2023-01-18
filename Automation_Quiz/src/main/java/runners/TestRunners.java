00766fa850f3cf2fb31fb8ac1a061100bbab6daf
preeti21885 <122968055+preeti21885@users.noreply.github.com>
2023-01-18 10:41:28 -0800

    code cleanup

--- Automation_Quiz/src/main/java/runners/TestRunners.java
+++ Automation_Quiz/src/main/java/runners/TestRunners.java
@@ -1,6 +1,5 @@
 package runners;
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
--- Automation_Quiz/src/main/java/stepDefinitions/StepDefintions.java
+++ Automation_Quiz/src/main/java/stepDefinitions/StepDefintions.java
@@ -1,22 +1,15 @@
 package stepDefinitions;
 
 import java.time.Year;
-import java.util.concurrent.TimeUnit;
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
